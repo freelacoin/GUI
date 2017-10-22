49c48589c6923850d3264f9d189d8d74f2177eb9
FreelaBit <31524456+freelacoin@users.noreply.github.com>
2017-10-22 19:57:40 -0200

    Add files via upload

---
+++ cryptonote/src/P2p/P2pNetworks.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2011-2017 The Cryptonote developers
+// Copyright (c) 2014-2017 XDN developers
+// Copyright (c) 2016-2017 BXC developers
+// Copyright (c) 2017 Royalties developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x8D, 0x6F, 0x44, 0x32, 0x56, 0xC1, 0x44, 0x7C, 0x3F, 0x90, 0x2F, 0x7C, 0x28, 0x4A, 0x36, 0x5D } };
+}
